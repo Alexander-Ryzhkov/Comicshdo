8694eebafb0e17b3982528af24a211db9240ca27
Denis <denis.balashevich@gmail.com>
2016-05-12 20:57:35 +0300

    test multi-design

--- src/Comicshdo/Controllers/HomeController.cs
+++ src/Comicshdo/Controllers/HomeController.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
-
+using Microsoft.AspNet.Http;
 namespace Comicshdo.Controllers
 {
     public class HomeController : Controller
     {
+        
         public IActionResult Index()
         {
+            
             return View();
         }
 

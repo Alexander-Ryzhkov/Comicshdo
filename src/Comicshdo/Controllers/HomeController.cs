271a24ef02816323fa6bde814953fb60091287cd
Denis <denis.balashevich@gmail.com>
2016-05-12 21:43:31 +0300

    в

--- src/Comicshdo/Controllers/HomeController.cs
+++ src/Comicshdo/Controllers/HomeController.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Authorization;
 
 namespace Comicshdo.Controllers
 {
     public class HomeController : Controller
     {
+        
         public IActionResult Index()
         {
+            
             return View();
         }
 
+        [Authorize(Roles = "Admin")]
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
